bec10d26e150abd39c474e098df66844c924ccfc
Charlie Friend <charles.d.friend@gmail.com>
2018-09-19 21:11:21 -0700

    Don't get firebase in app factory

--- scouting/__init__.py
+++ scouting/__init__.py
@@ -6,8 +6,6 @@ from scouting.routes import register_routes
 def create_app():
     app = Flask(__name__)
 
-    with app.app_context():
-        get_firebase()
     register_routes(app)
 
     return app

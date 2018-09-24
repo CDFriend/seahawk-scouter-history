c22f8f215a773652b9ef1ef5b56d9ba3fe964fea
Charlie Friend <charles.d.friend@gmail.com>
2018-09-23 22:05:13 -0700

    Parse event dates from VexDB.

--- scouting/models/event.py
+++ scouting/models/event.py
@@ -1,3 +1,5 @@
+import re
+from time import strptime, strftime
 from scouting.utils import get_location_str
 
 
@@ -10,7 +12,16 @@ class Event:
         self.region = str(kwargs["loc_region"])
         self.country = str(kwargs["loc_country"])
 
-        # TODO: parse date
+        # Bit of a hack so strptime can parse the date strings from VexDB.
+        # For some reason, time zones in VexDB strings are sent as +HH:MM,
+        # rather than +HHMM, the usual format. Uses a regex to make the time
+        # zone string from VexDB cooperate with strptime.
+        start_time = str(kwargs["start"])
+        start_time = re.sub("\+(\d+):(\d+)$", "+\g<1>\g<2>", start_time)
+        self.date = strptime(start_time, "%Y-%m-%dT%H:%M:%S%z")
 
     def get_location_str(self):
         return get_location_str(self.city, self.region, self.country)
+
+    def get_date_str(self):
+        return strftime("%B %d, %Y", self.date)

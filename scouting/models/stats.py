ffd37c0e194382a3ecc73ef5d442279fa66f3426
Charlie Friend <charles.d.friend@gmail.com>
2018-10-08 09:10:14 -0700

    Round autonomous point percent

--- scouting/models/stats.py
+++ scouting/models/stats.py
@@ -4,4 +4,4 @@ class Stats:
         self.avg_score = int(round(kwargs["avg_score"]))
         self.best_score = int(kwargs["best_score"])
         self.worst_score = int(kwargs["worst_score"])
-        self.auton_point_percent = float(kwargs["auton_point_percent"])
+        self.auton_point_percent = int(round(kwargs["auton_point_percent"]))
